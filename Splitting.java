eeee7c0f2cc8a2664dc8bfb65baf576a458eaab6
Aidar Takenov <aidartaken@gmail.com>
2024-12-13 23:33:27 +0600

    HackerRank task 15

---
+++ Splitting.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Splitting {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        scan.close();
+
+        String[] tokens = s.split("[^A-Za-z]+");
+
+        List<String> validTokens = new ArrayList<>();
+        for (String token : tokens) {
+            if (!token.isEmpty()) {
+                validTokens.add(token);
+            }
+        }
+
+        System.out.println(validTokens.size());
+
+        for (String token : validTokens) {
+            System.out.println(token);
+        }
+    }
+}
